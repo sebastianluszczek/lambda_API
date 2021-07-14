5114d3fc714f9be310d4c69f5ebf41c36a1b7eef
Sebastian Łuszczek <sebastianluszczek@pop-os.localdomain>
2021-07-14 11:54:26 +0200

    terraform clean-up

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -9,52 +9,52 @@ resource "aws_s3_bucket" "sls_api_storage" {
 
 # pack lambdas locally
 data "archive_file" "local_zip_getUsers" {
-  source_file = "${path.module}/dist/getUsers.js"
+  source_file = "../${path.root}/dist/getUsers.js"
   type = "zip"
-  output_path = "${path.module}/dist/getUsers.zip"
+  output_path = "${path.root}/dist/getUsers.zip"
 }
 
 data "archive_file" "local_zip_getUser" {
-  source_file = "${path.module}/dist/getUser.js"
+  source_file = "${path.root}/dist/getUser.js"
   type = "zip"  
-  output_path = "${path.module}/dist/getUser.zip"
+  output_path = "${path.root}/dist/getUser.zip"
 }
 
 data "archive_file" "local_zip_createUser" {
-  source_file = "${path.module}/dist/createUser.js"
+  source_file = "${path.root}/dist/createUser.js"
   type = "zip"
-  output_path = "${path.module}/dist/createUser.zip"
+  output_path = "${path.root}/dist/createUser.zip"
 }
 
 data "archive_file" "local_zip_deleteUser" {
-  source_file = "${path.module}/dist/deleteUser.js"
+  source_file = "${path.root}/dist/deleteUser.js"
   type = "zip"
-  output_path = "${path.module}/dist/deleteUser.zip"
+  output_path = "${path.root}/dist/deleteUser.zip"
 }
 
 # upload files to S3 bucket
 resource "aws_s3_bucket_object" "zip_getUsers" {
   bucket = "${aws_s3_bucket.sls_api_storage.bucket}"
   key = "getUsers.zip"
-  source = "${path.module}/dist/getUsers.zip"
+  source = "${path.root}/dist/getUsers.zip"
 }
 
 resource "aws_s3_bucket_object" "zip_getUser" {
   bucket = "${aws_s3_bucket.sls_api_storage.bucket}"
   key = "getUser.zip"
-  source = "${path.module}/dist/getUser.zip"
+  source = "${path.root}/dist/getUser.zip"
 }
 
 resource "aws_s3_bucket_object" "zip_createUser" {
   bucket = "${aws_s3_bucket.sls_api_storage.bucket}"
   key = "createUser.zip"
-  source = "${path.module}/dist/createUser.zip"
+  source = "${path.root}/dist/createUser.zip"
 }
 
 resource "aws_s3_bucket_object" "zip_deleteUser" {
   bucket = "${aws_s3_bucket.sls_api_storage.bucket}"
   key = "deleteUser.zip"
-  source = "${path.module}/dist/deleteUser.zip"
+  source = "${path.root}/dist/deleteUser.zip"
 }
 
 # create lambdas
